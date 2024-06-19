7746a53c4fc65c3f6113c10f3afb49085a6f5e6b
Santiago Rodriguez <santiagorrodriguez9@gmail.com>
2024-06-18 19:36:16 -0500

    Agregamos los estilos al archivo App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Testimonio from './componentes/Testimonio';
 function App() {
   return (
     <div className="App">
+      <div className='contenedor-principal'>
+      <h1> Esto es lo que dicen nuestros alumnos:</h1>
      <Testimonio/>
+     </div>
     </div>
   );
 }
